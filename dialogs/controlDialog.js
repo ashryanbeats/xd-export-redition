648b6a9aeef4585ce3acbd5a63944d6e4e6a1fa7
Ash <ash@ashryanbeats.com>
2019-02-18 11:00:00 -0500

    Breaking event listeners into their own function.

--- dialogs/controlDialog.js
+++ dialogs/controlDialog.js
@@ -16,9 +16,9 @@ async function getResultFromControlDialog(results, languageCode) {
   return await dialog.showModal();
 }
 
-async function getControlDialog(resultStrings, languageCode, prefs) {
+async function getControlDialog(resultStrings, languageCode, intialPrefs) {
   const imageTypeOptions = getImageTypeOptions();
-  const imageTypePref = prefs.renditionType;
+  const imageTypePref = intialPrefs.renditionType;
 
   // HTML markup
   document.body.innerHTML = `
@@ -39,7 +39,9 @@ async function getControlDialog(resultStrings, languageCode, prefs) {
         <label class="row row-wrapper">
           <span>File name</span>
           <input id="file-name" type="text" uxp-quiet="false" placeholder="Enter a file name" ${
-            prefs.filename.length ? `value="${prefs.filename}"` : null
+            intialPrefs.filename.length
+              ? `value="${intialPrefs.filename}"`
+              : null
           } />
         </label>
         <label class="row row-wrapper">
@@ -53,16 +55,16 @@ async function getControlDialog(resultStrings, languageCode, prefs) {
         <label>
           <div class="row spread">
             <span>Render scale</span>
-            <span id="scale-display-value">${prefs.scale}</span>
+            <span id="scale-display-value">${intialPrefs.scale}</span>
           </div>
           <input id="scale-range" type="range" min=1 max=5 step=1 value=${
-            prefs.scale
+            intialPrefs.scale
           } />
         </label>
         <label class="row row-wrapper">
           <span>Overwrite existing file</span>
           <input type="checkbox" id="overwrite-file"/ ${
-            prefs.overwriteFile ? "checked" : ""
+            intialPrefs.overwriteFile ? "checked" : ""
           }>
         </label>
         <footer>
@@ -78,8 +80,12 @@ async function getControlDialog(resultStrings, languageCode, prefs) {
   `;
   setSelectedFileType(imageTypeOptions, imageTypePref);
 
+  return getControlDialogWithEventHandlers(intialPrefs);
+}
+
+function getControlDialogWithEventHandlers(intialPrefs) {
   // Add event handlers
-  const [dialog, form, range, cancelButton, okButton] = [
+  const [dialog, form, scaleRange, cancelButton, okButton] = [
     "#control-dialog",
     "#control-dialog form",
     "#scale-range",
@@ -87,22 +93,17 @@ async function getControlDialog(resultStrings, languageCode, prefs) {
     "#ok-button"
   ].map(s => document.querySelector(s));
 
-  cancelButton.addEventListener("click", e => dialog.close("reasonCanceled"));
-  okButton.addEventListener("click", e => handleSubmit(e, dialog, prefs));
-  form.onsubmit = e => handleSubmit(e, dialog, prefs);
+  cancelButton.addEventListener("click", _ => dialog.close("reasonCanceled"));
+  okButton.addEventListener("click", e => handleSubmit(e, dialog, intialPrefs));
+  form.onsubmit = e => handleSubmit(e, dialog, intialPrefs);
 
-  const displayValue = document.querySelector("#scale-display-value");
-  range.addEventListener(
+  const scaleDisplayValue = document.querySelector("#scale-display-value");
+  scaleRange.addEventListener(
     "change",
-    e => (displayValue.textContent = e.target.value)
+    e => (scaleDisplayValue.textContent = e.target.value)
   );
 
-  // Show the modal
-  try {
-    return dialog;
-  } catch (err) {
-    console.log(err.message);
-  }
+  return dialog;
 }
 
 // Workaround since dropdowns don't support `selected` / `disabled` yet
@@ -112,24 +113,24 @@ function setSelectedFileType(imageTypeOptions, imageTypePref) {
   select.selectedIndex = imageTypeOptions.indexOf(imageTypePref);
 }
 
-async function handleSubmit(e, dialog, currentPrefs) {
-  saveAndClose(dialog, currentPrefs);
+async function handleSubmit(e, dialog, intialPrefs) {
+  saveAndClose(dialog, intialPrefs);
   e.preventDefault();
 }
 
-async function saveAndClose(dialog, currentPrefs) {
-  const savedPrefs = await savePrefs(currentPrefs);
+async function saveAndClose(dialog, intialPrefs) {
+  const savedPrefs = await savePrefs(intialPrefs);
 
   return await dialog.close(savedPrefs);
 }
 
-async function savePrefs(currentPrefs) {
-  const prefsToSave = getSettingsFromForm(currentPrefs);
+async function savePrefs(intialPrefs) {
+  const prefsToSave = getSettingsFromForm(intialPrefs);
   return await createPrefs(prefsToSave);
 }
 
-function getSettingsFromForm(currentPrefs) {
-  const [filenameInput, select, range, overwriteFile] = [
+function getSettingsFromForm(intialPrefs) {
+  const [filenameInput, filetypeSelect, scaleRange, overwriteFile] = [
     "#file-name",
     "#file-type-select",
     "#scale-range",
@@ -137,10 +138,10 @@ function getSettingsFromForm(currentPrefs) {
   ].map(sel => document.querySelector(sel));
 
   return {
-    ...currentPrefs,
-    renditionType: select.value,
+    ...intialPrefs,
+    renditionType: filetypeSelect.value,
     overwriteFile: overwriteFile.checked ? true : false,
-    scale: Number(range.value),
+    scale: Number(scaleRange.value),
     filename: filenameInput.value
   };
 }
