4589263740d33b576f0f9f8b59fed742ac2d9858
Ash <ash@ashryanbeats.com>
2019-02-18 14:25:35 -0500

    Added some JSDoc.

--- dialogs/controlDialog.js
+++ dialogs/controlDialog.js
@@ -5,23 +5,33 @@ const {
   getImageTypeOptions
 } = require("../file-handlers/prefs.js");
 
+/**
+ * Shows the control dialog modal, passing back any return value from the modal.
+ * @param {Object} strings - An object containing UI strings for the modal.
+ * @param {application.appLanguage} languageCode - The current language the application UI is using.
+ * @returns {(string|Object)} Error string or Object containing settings for the render.
+ */
 async function getResultFromControlDialog(strings, languageCode) {
-  const intialPrefs = await getPrefs();
+  const initialPrefs = await getPrefs();
 
   const dialog = await getControlDialog(
     strings.controls,
     languageCode,
-    intialPrefs
+    initialPrefs
   );
 
   return await dialog.showModal();
 }
 
-async function getControlDialog(strings, languageCode, intialPrefs) {
+/**
+ * Appends the dialog to the DOM.
+ * @param {Object} strings - An object containing UI strings for the modal.
+ * @param {application.appLanguage} languageCode - The current language the application UI is using.
+ * @param {Object} initialPrefs - An object of preference settings currently stored in prefs.json.
+ * @returns {HTMLDialogElement} The control dialog element.
+ */
+async function getControlDialog(strings, languageCode, initialPrefs) {
   const imageTypeOptions = getImageTypeOptions();
-  const imageTypePref = intialPrefs.renditionType;
-
-  console.log(strings);
 
   // HTML markup
   document.body.innerHTML = `
@@ -34,13 +44,13 @@ async function getControlDialog(strings, languageCode, intialPrefs) {
           <input id="file-name" type="text" uxp-quiet="false" placeholder="${
             strings[languageCode].filenamePlaceholder
           }" ${
-    intialPrefs.filename.length ? `value="${intialPrefs.filename}"` : null
+    initialPrefs.filename.length ? `value="${initialPrefs.filename}"` : null
   } />
         </label>
         <label class="row row-wrapper">
           <span>${strings[languageCode].renditionTypeLabel}</span>
           <select id="file-type-select">
-            ${getImageTypeOptions().map(el => {
+            ${imageTypeOptions.map(el => {
               return `<option value="${el}">${el.toUpperCase()}</option>`;
             })}
           </select>
@@ -48,16 +58,16 @@ async function getControlDialog(strings, languageCode, intialPrefs) {
         <label>
           <div class="row spread">
             <span>${strings[languageCode].renderScaleLabel}</span>
-            <span id="scale-display-value">${intialPrefs.scale}x</span>
+            <span id="scale-display-value">${initialPrefs.scale}x</span>
           </div>
           <input id="scale-range" type="range" min=1 max=5 step=1 value=${
-            intialPrefs.scale
+            initialPrefs.scale
           } />
         </label>
         <label class="row row-wrapper">
           <span>${strings[languageCode].overwriteFileLabel}</span>
           <input type="checkbox" id="overwrite-file"/ ${
-            intialPrefs.overwriteFile ? "checked" : ""
+            initialPrefs.overwriteFile ? "checked" : ""
           }>
         </label>
         <footer>
@@ -71,12 +81,17 @@ async function getControlDialog(strings, languageCode, intialPrefs) {
       </form>
     </dialog>
   `;
-  setSelectedFileType(imageTypeOptions, imageTypePref);
+  setSelectedFileType(imageTypeOptions, initialPrefs.renditionType);
 
-  return getControlDialogWithEventHandlers(intialPrefs);
+  return getControlDialogWithEventHandlers(initialPrefs);
 }
 
-function getControlDialogWithEventHandlers(intialPrefs) {
+/**
+ * Adds the required event handlers to the control dialog.
+ * @param {Object} initialPrefs - An object of preference settings currently stored in prefs.json.
+ * @returns {HTMLDialogElement} The control dialog element.
+ */
+function getControlDialogWithEventHandlers(initialPrefs) {
   // Add event handlers
   const [dialog, form, scaleRange, cancelButton, okButton] = [
     "#control-dialog",
@@ -86,10 +101,18 @@ function getControlDialogWithEventHandlers(intialPrefs) {
     "#ok-button"
   ].map(s => document.querySelector(s));
 
+  // Close dialog when cancel is clicked.
+  // Note that XD handles the ESC key for you, also returning `reasonCanceled`
   cancelButton.addEventListener("click", _ => dialog.close("reasonCanceled"));
-  okButton.addEventListener("click", e => handleSubmit(e, dialog, intialPrefs));
-  form.onsubmit = e => handleSubmit(e, dialog, intialPrefs);
 
+  // Handle ok button click
+  okButton.addEventListener("click", e =>
+    handleSubmit(e, dialog, initialPrefs)
+  );
+  // Handle form submit via return key
+  form.onsubmit = e => handleSubmit(e, dialog, initialPrefs);
+
+  // Update display value for render scale when the range is changed.
   const scaleDisplayValue = document.querySelector("#scale-display-value");
   scaleRange.addEventListener(
     "change",
@@ -99,30 +122,46 @@ function getControlDialogWithEventHandlers(intialPrefs) {
   return dialog;
 }
 
-// Workaround since dropdowns don't support `selected` / `disabled` yet
-// https://adobexdplatform.com/plugin-docs/reference/ui/elements/dropdowns.html#known-issues
+/**
+ * Sets the selected file type in the dropdown according to store preferences.
+ * This is a workaround since UXP dropdowns don't yet support `selected` / `disabled`.
+ * https://adobexdplatform.com/plugin-docs/reference/ui/elements/dropdowns.html#known-issues
+ * @param {application.RenditionType[]} imageTypeOptions - All XD-supported image types for render.
+ * @param {string} imageTypePref - The preference setting for rendition type currently stored in prefs.json.
+ */
 function setSelectedFileType(imageTypeOptions, imageTypePref) {
   const select = document.querySelector("#file-type-select");
   select.selectedIndex = imageTypeOptions.indexOf(imageTypePref);
 }
 
-async function handleSubmit(e, dialog, intialPrefs) {
-  saveAndClose(dialog, intialPrefs);
+async function handleSubmit(e, dialog, initialPrefs) {
+  saveAndClose(dialog, initialPrefs);
   e.preventDefault();
 }
 
-async function saveAndClose(dialog, intialPrefs) {
-  const savedPrefs = await savePrefs(intialPrefs);
+/**
+ * Saves preferences and closes the dialog, passing back those saved preferences.
+ * @param {HTMLDialogElement} dialog The control dialog element.
+ * @param {Object} initialPrefs - An object of preference settings currently stored in prefs.json.
+ * @returns {Object} Updated preferences stored in prefs.json.
+ */
+async function saveAndClose(dialog, initialPrefs) {
+  const savedPrefs = await savePrefs(initialPrefs);
 
   return await dialog.close(savedPrefs);
 }
 
-async function savePrefs(intialPrefs) {
-  const prefsToSave = getSettingsFromForm(intialPrefs);
+async function savePrefs(initialPrefs) {
+  const prefsToSave = getSettingsFromForm(initialPrefs);
   return await createPrefs(prefsToSave);
 }
 
-function getSettingsFromForm(intialPrefs) {
+/**
+ * Gets preferences to save by retrieving setting values from the DOM.
+ * @param {Object} initialPrefs - An object of preference settings currently stored in prefs.json.
+ * @returns {Object} Rendition setting values retrieved from DOM.
+ */
+function getSettingsFromForm(initialPrefs) {
   const [filenameInput, filetypeSelect, scaleRange, overwriteFile] = [
     "#file-name",
     "#file-type-select",
@@ -131,7 +170,7 @@ function getSettingsFromForm(intialPrefs) {
   ].map(sel => document.querySelector(sel));
 
   return {
-    ...intialPrefs,
+    ...initialPrefs,
     renditionType: filetypeSelect.value,
     overwriteFile: overwriteFile.checked ? true : false,
     scale: Number(scaleRange.value),
--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const { showResultDialog } = require("./dialogs/resultDialog.js");
 const { renderToFile } = require("./file-handlers/render.js");
 const { getPrefs } = require("./file-handlers/prefs.js");
 
+/**
+ * Initiates the plugin by performing selection check and running the control dialog.
+ * @param {selection} selection - The currently selected node in XD'd UI.
+ */
 async function initiatePlugin(selection) {
   const languageCode = application.appLanguage;
 
@@ -12,17 +16,21 @@ async function initiatePlugin(selection) {
   if (selection.items.length === 0)
     return showResultDialog(strings.errorNoSelection, languageCode);
 
+  // Get rendition settings from the control dialog
   let dialogResult = await getResultFromControlDialog(strings, languageCode);
   if (dialogResult === "reasonCanceled") return;
 
   return exportRendition(selection, dialogResult, languageCode);
 }
 
-// The main plugin function.
-// Returns a dialog that communicates the outcome of running the plugin to the user.
+/**
+ * Shows the results dialog modal that communicates the outcome of running the plugin to the user.
+ * @param {selection} selection - The currently selected node in XD'd UI.
+ * @param {Object} dialogResult - The results of successfully running the control dialog. An Object containing settings for the render.
+ * @param {application.appLanguage} languageCode - The current language the application UI is using.
+ */
 async function exportRendition(selection, dialogResult, languageCode) {
   // Try to get rendition results for the first item in the selection.
-  // Exit if there is an error encountered along the way.
   try {
     const selectionItemToRender = selection.items[0];
     const renditionResults = await renderToFile(
@@ -33,11 +41,14 @@ async function exportRendition(selection, dialogResult, languageCode) {
     // Success! Let the user know!
     return showResultDialog(strings.success, languageCode, renditionResults);
   } catch (err) {
+    // Exit if there is an error encountered along the way.
     const prefs = await getPrefs();
     console.log("[Error]", err.message);
     console.log("prefs", prefs);
 
+    // Skip the results dialog if the user has set the skip preference.
     if (err.message === "errorNoFolder" && prefs.skipNoFolderMessage) return;
+    // Error! Let the user know!
     return showResultDialog(strings[err.message], languageCode);
   }
 }
